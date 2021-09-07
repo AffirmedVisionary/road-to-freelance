eb4ea57f231e83310b247baef5d95d494cbe53c8
Charlene Grant <68870508+AffirmedVisionary@users.noreply.github.com>
2021-09-07 14:07:04 +0100

    striping in progress

--- diary-front-end/src/App.js
+++ diary-front-end/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://affirmedvisionary.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit me!
         </a>
       </header>
     </div>
