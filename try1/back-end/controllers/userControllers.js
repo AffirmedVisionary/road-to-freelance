e4da63ca8a0ead1610afccdbe306a5e6b0e15ae6
Charlene Grant <68870508+AffirmedVisionary@users.noreply.github.com>
2021-09-10 14:52:13 +0100

    working register and login

--- try1/back-end/controllers/userControllers.js
+++
@@ -1,5 +0,0 @@
-const User = require("../models/user");
-
-exports.read = (req, res) => {
-  res.send({ message: `hello ${User}` });
-};
--- try1/diary-front-end/src/config/devConfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  publicRuntimeConfig: {
-    APP_NAME: "VISIONARYJOURNAL",
-    API_DEVELOPMENT: "http://localhost:8000/api",
-    API_PRODUCTION: false,
-  },
-};
